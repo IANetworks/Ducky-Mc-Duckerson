6f834f0654908b0214221e046313b3203a565326
AbbyTheRat <ad@vr-networks.co.uk>
2017-12-31 23:49:48 -0500

    ref: #9 basic image profile is done. Added the last set of games infomation.
    Signed-off-by: AbbyTheRat <ad@vr-networks.co.uk>

--- src/main/java/io/github/mannjamin/ducky/commandstructure/NewProfileCS.java
+++ src/main/java/io/github/mannjamin/ducky/commandstructure/NewProfileCS.java
@@ -95,19 +95,22 @@ public class NewProfileCS extends CommandStructure {
             up = dbMan.getUserProfile(member.getGuild().getIdLong(), member.getUser().getIdLong());
             //obtain all the strings.. ALL the strings
             String title = up.getTitle();
-            String rank = up.getRankName();
+            String rankName = up.getRankName();
             String guildName = member.getGuild().getName();
             String balance = up.getBalance().toString();
             String tableFlip = up.getFlipped().toString();
             String tableUnflip = up.getUnflipped().toString();
             String werewolfWins = up.getWerewolfWins().toString();
             String werewolfGames = up.getWerewolfGames().toString();
+            //TODO TotalGames/TotalGamesWon
+            String totalWins = up.getGameWins().toString();
+            String totalGames = up.getTotalGames().toString();
             String rankExp = up.getRankExp().toString();
             String rankExpTotal;
             if (up.getRankExp() != null)
-                rankExpTotal = rankExp + "/" + up.getLevel().toString();
+                rankExpTotal = up.getLevel().toString() + " / " + rankExp;
             else
-                rankExpTotal = rankExp + "/∞";
+                rankExpTotal = up.getLevel().toString() + " / ∞";
             String points = up.getPoints().toString();
             String tableFlipStr = "(╯°□°）╯︵ ┻━┻";
             String unflipStr = "┬─┬\uFEFF ノ( ゜-゜ノ)";
@@ -181,10 +184,12 @@ public class NewProfileCS extends CommandStructure {
             int statTagBackgroundHeight = (int) Math.round(statTag.getHeight() + 10);
             int statTagX = (int) Math.round((profileBackgroundImg.getWidth() - statTag.getWidth()) / 2);
 
-            int maxStatTagWidth = 0;
-            int testMaxStatTagWidth = 0;
+            int maxStatTagWidth;
+            int maxGameTagWidth;
+            int testMaxStatTagWidth;
+            int testMaxGameTagWidth;
             //RankTag Locations
-            canvas.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 14));
+            canvas.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 12));
             fontMetrics = canvas.getFontMetrics();
             Rectangle2D rankTag = fontMetrics.getStringBounds("Rank :", canvas);
             int rankTagY = statTagYDrop + statTagBackgroundHeight + 5;
@@ -194,31 +199,70 @@ public class NewProfileCS extends CommandStructure {
             int curTagY = rankTagY + (int) Math.floor(rankTag.getHeight());
             Rectangle2D curTag = fontMetrics.getStringBounds("Coins :", canvas);
             testMaxStatTagWidth = (int) Math.floor(curTag.getWidth()) + padding;
-            if (testMaxStatTagWidth > maxStatTagWidth) maxStatTagWidth = testMaxStatTagWidth;
-
+            maxStatTagWidth = Math.max(testMaxStatTagWidth, maxStatTagWidth);
 
             int tableFlipTagY = curTagY + (int) Math.floor(rankTag.getHeight());
             Rectangle2D flipTag = fontMetrics.getStringBounds(tableFlipStr + " :", canvas);
             testMaxStatTagWidth = (int) Math.floor(flipTag.getWidth()) + padding;
-            if (testMaxStatTagWidth > maxStatTagWidth) maxStatTagWidth = testMaxStatTagWidth;
-
+            maxStatTagWidth = Math.max(testMaxStatTagWidth, maxStatTagWidth);
 
             int unFlipTableTagY = tableFlipTagY + (int) Math.floor(rankTag.getHeight());
             Rectangle2D unflipTag = fontMetrics.getStringBounds(unflipStr + " :", canvas);
             testMaxStatTagWidth = (int) Math.floor(unflipTag.getWidth()) + padding;
-            if (testMaxStatTagWidth > maxStatTagWidth) maxStatTagWidth = testMaxStatTagWidth;
-
+            maxStatTagWidth = Math.max(testMaxStatTagWidth, maxStatTagWidth);
 
             int rankTagX = maxStatTagWidth - (int) Math.floor(rankTag.getWidth());
             int curTagX = maxStatTagWidth - (int) Math.floor(curTag.getWidth());
             int tableFlipTagX = maxStatTagWidth - (int) Math.floor(flipTag.getWidth());
             int unFlipTableTagX = maxStatTagWidth - (int) Math.floor(unflipTag.getWidth());
 
+            int realStatX = maxStatTagWidth + 5;
+
+            //Games location
+            canvas.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 14));
+            fontMetrics = canvas.getFontMetrics();
+            Rectangle2D gameTag = fontMetrics.getStringBounds("Games", canvas);
+            int gameTagYBG = unFlipTableTagY + (int) unflipTag.getHeight() + 2;
+            int gameTagYDrop = gameTagYBG + (int) Math.round(gameTag.getHeight());
+            int gameTagBackgroundHeight = (int) Math.round(gameTag.getHeight() + 10);
+            int gameTagX = (int) Math.round((profileBackgroundImg.getWidth() - gameTag.getWidth()) / 2);
+
+            //Games Stat
+            canvas.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 12));
+            fontMetrics = canvas.getFontMetrics();
+            Rectangle2D werewolfTag = fontMetrics.getStringBounds("Werewolf :", canvas);
+
+            int werewolfTagY = gameTagYDrop + gameTagBackgroundHeight + 5;
+            maxGameTagWidth = (int) Math.floor(werewolfTag.getWidth()) + padding;
+
+            int totalGameTagY = werewolfTagY + (int) werewolfTag.getHeight();
+            Rectangle2D totalGamesTag = fontMetrics.getStringBounds("Total Games :", canvas);
+            testMaxGameTagWidth = (int) totalGamesTag.getWidth() + padding;
+            maxGameTagWidth = Math.max(testMaxGameTagWidth, maxGameTagWidth);
+
+            int pointGameTagY = totalGameTagY + (int) werewolfTag.getHeight();
+            Rectangle2D pointsGameTag = fontMetrics.getStringBounds("Points :", canvas);
+            testMaxGameTagWidth = (int) pointsGameTag.getWidth() + padding;
+            maxGameTagWidth = Math.max(testMaxGameTagWidth, maxGameTagWidth);
+
+
+            int werewolfTagX = maxGameTagWidth - (int) Math.floor(werewolfTag.getWidth());
+            int totalGamesTagX = maxGameTagWidth - (int) Math.floor(totalGamesTag.getWidth());
+            int pointGameTagX = maxGameTagWidth - (int) Math.floor(pointsGameTag.getWidth());
+
+            int realGamesX = maxGameTagWidth + 5;
+
+            //Content Background fill
+            int contentBackgroundWidth = profileBackgroundImg.getWidth();
+            int contentBackGroundHeight = profileBackgroundImg.getHeight() - statTagYBG - ((int) userLevelTag.getHeight() * 2);
+
             //place all elements onto canvas
             Color nameTagColour = new Color(0, 0, 0, 64);
             canvas.setColor(nameTagColour);
+            canvas.fillRect(0, statTagYBG, contentBackgroundWidth, contentBackGroundHeight);
             canvas.fillRoundRect(centerXNameTagBG, nameTagY, nameTagBackgroundWidth, nameTagBackgroundHeight, 3, 3);
             canvas.fillRoundRect(centerXNameTagBG, statTagYBG, nameTagBackgroundWidth, statTagBackgroundHeight, 3, 3);
+            canvas.fillRoundRect(centerXNameTagBG, gameTagYBG, nameTagBackgroundWidth, gameTagBackgroundHeight, 3, 3);
             //Images
             canvas.drawImage(avatar, rightJustifiedAvatar, heightDropAvatar, null);
 
@@ -229,17 +273,29 @@ public class NewProfileCS extends CommandStructure {
             //Font Size 14
             canvas.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 14));
             canvas.drawString("Statistic", statTagX, statTagYDrop);
+            canvas.drawString("Games", gameTagX, gameTagYDrop);
             //Font Size 12
             canvas.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 12));
             canvas.drawString("Rank :", rankTagX, rankTagY);
             canvas.drawString("Coins :", curTagX, curTagY);
             canvas.drawString(tableFlipStr + " :", tableFlipTagX, tableFlipTagY);
             canvas.drawString(unflipStr + " :", unFlipTableTagX, unFlipTableTagY);
-
+            canvas.drawString("Werewolf :", werewolfTagX, werewolfTagY);
+            canvas.drawString("Total Games :", totalGamesTagX, totalGameTagY);
+            canvas.drawString("Points :", pointGameTagX, pointGameTagY);
+            //Font plain text - 12
+            canvas.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 12));
+            canvas.drawString(rankName + " (" + rankExpTotal + ")", realStatX, rankTagY);
+            canvas.drawString(balance, realStatX, curTagY);
+            canvas.drawString(tableFlip, realStatX, tableFlipTagY);
+            canvas.drawString(tableUnflip, realStatX, unFlipTableTagY);
+            canvas.drawString(werewolfWins + " / " + werewolfGames, realGamesX, werewolfTagY);
+            canvas.drawString(totalWins + " / " + totalGames, realGamesX, totalGameTagY);
+            canvas.drawString(points, realGamesX, pointGameTagY);
 
             //Font Size 10
             canvas.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 10));
-            canvas.drawString(userLevelName, padding, userLevelY);
+            canvas.drawString(userLevelName, userLevelX, userLevelY);
             canvas.drawString(guildName, guildNameX, userLevelY);
             if (hasTitle) {
 
